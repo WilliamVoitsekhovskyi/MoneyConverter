e953a5fa055db2da10e3f6a9cfe0d9b2c6f918f6
AntonBilous <49946128+AntonBilous@users.noreply.github.com>
2019-09-09 16:19:35 +0300

    ver 0.6.6

--- app/src/main/java/com/example/moneyonverter/MainActivity.java
+++ app/src/main/java/com/example/moneyonverter/MainActivity.java
@@ -2,57 +2,84 @@ package com.example.moneyonverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText enteredValue;
+    EditText convertedValue;
+    TextView coefficient;
+    TextView updateInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9)
+        if (android.os.Build.VERSION.SDK_INT > 19)
         {
             StrictMode.ThreadPolicy policy = new
                     StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+        convertedValue = findViewById(R.id.resultView);
+        convertedValue.setFocusable(false);                                       // to make EditText field(resultView) uneditable
+
+
     }
 
     public void setConvertedValue(View view){
-        EditText enteredValue = findViewById(R.id.enteredValue);
-        TextView convertedValue = findViewById(R.id.resultView);
-        TextView coefficient = findViewById(R.id.coefficientView);
-        TextView textView = findViewById(R.id.tv_exeption);
-        TextView updateInfo = findViewById(R.id.UpdateView);
+        enteredValue = findViewById(R.id.enteredValue);
+        coefficient = findViewById(R.id.coefficientView);
+        updateInfo = findViewById(R.id.UpdateView);
         String number = "0";
-        if(isOnline(this)) {
-            if(enteredValue.getText().length() == 0) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "The field is empty!", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-            else {
-                textView.setText("");
-                number = enteredValue.getText().toString();
-            }
-            updateInfo.setText(Informer.getUpdateTime());
-            coefficient.setText(Informer.getExchangeRate() + " UAH = 1 USD");
-            convertedValue.setText(Informer.getExchangeResult(Double.valueOf(number)) + " UAH");
+        String UAH_USD = getString(R.string.UAH_USD);
+        switch (view.getId())
+        {
+            case R.id.convertButton:
+      //          if(isOnline(this)) {
+                    if(enteredValue.getText().length() == 0) {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "The field is empty!", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                    else {
+                        number = enteredValue.getText().toString();
+                    }
+                    updateInfo.setText(Informer.getUpdateTime());
 
-        }
-        else{
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "No internet connection", Toast.LENGTH_SHORT);
-            toast.show();
-        }
+                    String a = Informer.getExchangeRate() + UAH_USD;
+                    coefficient.setText(a);
+                    convertedValue.setText(Informer.getExchangeResult(Double.valueOf(number)) + " UAH");
+
+                }
+                if(isOnline(this)) {
+                    if(enteredValue.getText().length() == 0) {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "The field is empty!", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                    else {
+                        number = enteredValue.getText().toString();
+                    }
+                    updateInfo.setText(Informer.getUpdateTime());
+                    String currensyName = Informer.getExchangeRate() + UAH_USD;
+                    coefficient.setText(currensyName);
+                    convertedValue.setText(Informer.getExchangeResult(Double.valueOf(number)) + " UAH");
+
+                //}
+     }
     }
 
     public static boolean isOnline(Context context)
@@ -62,5 +89,10 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
     }
+
+
+
+
+
 
 }
