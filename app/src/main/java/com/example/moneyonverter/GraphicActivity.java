8411a67f0c6df16084f0db42921ee729a0ddf754
WilliamVoitsekhovskyi <45714708+WilliamVoitsekhovskyi@users.noreply.github.com>
2019-09-28 13:31:06 +0300

    0.11.11.1 -New design for GraphicActivity(Table added, scrollable)

---
+++ app/src/main/java/com/example/moneyonverter/GraphicActivity.java
@@ -0,0 +1,14 @@
+package com.example.moneyonverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GraphicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphic);
+    }
+}
