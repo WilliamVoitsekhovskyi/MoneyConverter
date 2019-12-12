2bb0314cffb8af53ba776993ba1e69cf5b334acb
WilliamVoitsekhovskyi <45714708+WilliamVoitsekhovskyi@users.noreply.github.com>
2019-12-12 15:44:55 +0200

    0.13.13.2 -Added zoom for rates

--- app/src/main/java/com/example/moneyonverter/GraphicActivity.java
+++ app/src/main/java/com/example/moneyonverter/GraphicActivity.java
@@ -1,10 +1,16 @@
 package com.example.moneyonverter;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Spinner;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -13,7 +19,35 @@ public class GraphicActivity extends AppCompatActivity {
 
     Spinner SP_currencySelected;
     Spinner SP_resultCurrencySelected;
+    private WebView webView;
 
+    private class MyWebViewClient extends WebViewClient {
+
+
+
+        @Override
+        public void onPageFinished(WebView view, String url)
+        {
+            webView.loadUrl ("javascript: document.getElementByClassName ('frame fm-middle');");
+        }
+
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+        // Для старых устройств
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +55,15 @@ public class GraphicActivity extends AppCompatActivity {
 
         SP_resultCurrencySelected = findViewById(R.id.changeResultCurrency);
         SP_resultCurrencySelected.setSelection(3);
+        final MyWebViewClient MyWebViewClient = new MyWebViewClient();
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(MyWebViewClient);
+        // включаем поддержку JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.floatrates.com/chart/usd/eur/");
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.scrollTo(0,1050);
+        //webView.clearFormData();
 
         CurrencyDataBaseHelper currencyDataBaseHelper = new CurrencyDataBaseHelper(this, "CurrencyRateDB", 1 );   // here should be (this) not (this, "", 1 )
         ContentValues contentValues = new ContentValues();
