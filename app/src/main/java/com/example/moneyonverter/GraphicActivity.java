392e53878fa8f1fc020f26f50a84b97d723b9de4
WilliamVoitsekhovskyi <45714708+WilliamVoitsekhovskyi@users.noreply.github.com>
2019-12-14 16:19:54 +0200

    v 1.1 -Design fixes -Added new launcher icon -Added the back button in the action bar in the GraphicActivity

--- app/src/main/java/com/example/moneyonverter/GraphicActivity.java
+++ app/src/main/java/com/example/moneyonverter/GraphicActivity.java
@@ -1,8 +1,10 @@
 package com.example.moneyonverter;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Spinner;
@@ -10,27 +12,32 @@ import android.widget.Spinner;
 import MoneyConverterData.CurrencyDataBaseHelper;
 import MoneyConverterJSON.WebViewer;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class GraphicActivity extends AppCompatActivity {
 
-    Spinner SP_currencySelected;
-    Spinner SP_resultCurrencySelected;
+    private Spinner SP_currencySelected;
+    private Spinner SP_resultCurrencySelected;
 
-    String currencyChoice;
-    String resultCurrencyChoice;
+
+
+    private ActionBar actionBar;
 
     private WebView webView;
 
 
 
+    @SuppressLint("RestrictedApi")
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphic);
+
+        setActionBarBackButton();
 
         initAllSpinners();
         SP_resultCurrencySelected.setSelection(3);
@@ -47,11 +54,19 @@ public class GraphicActivity extends AppCompatActivity {
 
         CurrencyDataBaseHelper currencyDataBaseHelper = new CurrencyDataBaseHelper(this, "CurrencyRateDB", 1 );   // here should be (this) not (this, "", 1 )
         ContentValues contentValues = new ContentValues();
+
     }
 
     private void initAllSpinners() {
         SP_currencySelected = findViewById(R.id.changeCurrency);
         SP_resultCurrencySelected = findViewById(R.id.changeResultCurrency);
+    }
+
+    private void setActionBarBackButton(){
+        actionBar = getSupportActionBar();
+
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     public void onClick_doReverse(View view){
@@ -66,6 +81,9 @@ public class GraphicActivity extends AppCompatActivity {
     }
 
     public void onClick_showGraphic(View view){
+        String currencyChoice;
+        String resultCurrencyChoice;
+
         initAllSpinners();
 
         currencyChoice = String.valueOf(SP_currencySelected.getSelectedItem());
@@ -73,5 +91,14 @@ public class GraphicActivity extends AppCompatActivity {
 
         WebViewer.getGraphic(webView, currencyChoice, resultCurrencyChoice);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
