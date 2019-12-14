91e70938d806a1d3f50294d03c5e5a2e43593e32
William Voitsekhovski <viliam2323@gmail.com>
2019-12-14 12:11:46 +0200

    v 1.0 -Packages -Added Class WebViewer(for working with the graphic) -The graphic supports all available currencies -Refactoring

---
+++ app/src/main/java/MoneyConverterJSON/WebViewer.java
@@ -0,0 +1,45 @@
+package MoneyConverterJSON;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewer extends WebViewClient {
+    @Override
+    public void onPageFinished(WebView webView, String url)
+    {
+        webView.loadUrl ("javascript: document.getElementByClassName ('frame fm-middle');");
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
+        return true;
+    }
+
+    // Для старых устройств
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+
+    public void prepareWebPage(WebView webView){
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        webView.scrollTo(0,1050);
+    }
+
+    public static void getGraphic(WebView webView, String currencyCode, String currencyResultCode){
+
+        currencyCode = currencyCode.toLowerCase();
+        currencyResultCode = currencyResultCode.toLowerCase();
+
+        webView.loadUrl("http://www.floatrates.com/chart/" + currencyCode + "/" + currencyResultCode + "/");
+    }
+}
